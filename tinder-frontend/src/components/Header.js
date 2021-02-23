f79e8a18d5eb7239060f8b0f5e9b53e156ed6a5d
kou25 <koustav.manna98@gmail.com>
2021-02-24 00:44:17 +0530

    added chat page

--- tinder-frontend/src/components/Header.js
+++ tinder-frontend/src/components/Header.js
@@ -3,21 +3,35 @@ import '../assests/css/Header.css'
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
 import ForumIcon from '@material-ui/icons/Forum';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import {Link} from "react-router-dom"
 
-export default function Header() {
+export default function Header({backButton}) {
     return (
         <div className='header'>
+            {backButton?(
+                <Link to={backButton}>
+                <IconButton>
+                    <ArrowBackIosIcon fontSize="large" className="header__icon" /> 
+               </IconButton>
+               </Link>
+            ):
             <IconButton>
                 <PersonIcon fontSize="large" className="header__icon" />
             </IconButton>
+            }
+            <Link to="/">
             <img
             className="header__logo"
             src="https://cdn.worldvectorlogo.com/logos/tinder-2.svg"
             alt="logo"
             />
+            </Link>
+            <Link to="/chats">
             <IconButton>
                 <ForumIcon fontSize="large" className="header__icon" />
             </IconButton>
+            </Link>
         </div>
     )
 }
